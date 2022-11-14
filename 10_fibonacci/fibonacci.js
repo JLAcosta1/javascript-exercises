ffd4bd6755ea7638f99683d0abfb362c78cfef66
JLAcosta1 <mathassistant3.14@gmail.com>
2022-11-13 22:55:02 -0700

    Finished fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(val) {
+    if(val < 0) {
+        return "OOPS"
+    }
+    let x = parseInt(val, 10);
 
+    if (x === 1) {
+        return 1;
+    }
+
+    else if (x === 2) {
+        return 1;
+    }
+    
+    return fibonacci(x - 1) + fibonacci(x - 2)
 };
 
 // Do not edit below this line
