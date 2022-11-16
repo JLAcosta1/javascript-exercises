d976ccd1c0a09561605b5f0599dab5793098e9b4
JLAcosta1 <mathassistant3.14@gmail.com>
2022-11-15 20:37:34 -0700

    Finished findTheOldest exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,25 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    let person = undefined
 
+    const Oldest = arr.reduce((oldest, a) => {
+        if (!('yearOfDeath' in a)) {
+            if ((((new Date()).getFullYear()) - a.yearOfBirth ) > oldest) {
+                oldest = ((new Date()).getFullYear()) - a.yearOfBirth;
+                person = a;
+            }
+        }
+
+        else {
+            if ((a.yearOfDeath - a.yearOfBirth) > oldest) {
+                oldest = a.yearOfDeath - a.yearOfBirth;
+                person = a;
+            }
+        }
+
+        return oldest;
+    }, 0);
+
+    return person;
 };
 
 // Do not edit below this line
